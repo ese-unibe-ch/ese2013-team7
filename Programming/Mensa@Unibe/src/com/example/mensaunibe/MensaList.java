14392485b82246707430ee092e48873d90749edf
Bugius <bugius1000@gmail.com>
2013-10-15 18:16:14 +0200

    Adding List
    Added a list and populated it with random values on homescreen

--- Programming/Mensa@Unibe/src/com/example/mensaunibe/MensaList.java
+++ Programming/Mensa@Unibe/src/com/example/mensaunibe/MensaList.java
@@ -1,10 +1,18 @@
 package com.example.mensaunibe;
 
-import com.example.mensaunibe.api.MensaData;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MensaList extends Activity {
 
@@ -13,10 +21,34 @@ public class MensaList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensa_list);
         
-        MensaData md = new MensaData();
-		System.out.println( md.getMensaList() );
+        List valueList = new ArrayList<String>();
+        for (int i = 0; i < 10; i++)
+        {
+        	valueList.add("value"+i);
+        }    
         
+        ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList)
+        {        
+        	@Override
+        	public View getView(int position, View convertView,
+        			ViewGroup parent) {
+        		View view =super.getView(position, convertView, parent);
+
+        		TextView textView=(TextView) view.findViewById(android.R.id.text1);
+
+        		/*YOUR CHOICE OF COLOR*/
+        		textView.setTextColor(Color.BLACK);
+
+        		return view;
+        	}
+        };
+        
+        final ListView lv = (ListView)findViewById(R.id.listView1);
+
+        lv.setAdapter(adapter);    
     }
+    
+    
 
 
     @Override
