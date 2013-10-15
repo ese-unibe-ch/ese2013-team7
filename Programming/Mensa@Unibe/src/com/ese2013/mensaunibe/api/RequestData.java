86b19a940694071c93c6187a65cd89d4dd324781
Bugius <bugius1000@gmail.com>
2013-10-15 22:35:03 +0200

    Cleaned

--- Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/api/RequestData.java
+++ Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/api/RequestData.java
@@ -8,31 +8,6 @@ import java.net.URL;
 
 import android.os.AsyncTask;
 
-
-/*import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import java.io.InputStream;*/
-
-/*
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import android.net.Uri;
-import android.net.Uri.Builder;
-*/
-
-
 public class RequestData extends AsyncTask<String, Void, String> {
 	private String url;
 	
@@ -69,38 +44,4 @@ public class RequestData extends AsyncTask<String, Void, String> {
 	protected String doInBackground(String... arg0) {
 		return this.request();
 	}
-	
-	/*public String request() {
-		InputStream is = null;
-		try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(this.url);
- 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            is = httpEntity.getContent();           
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-         
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-            return sb.toString();
-        } catch (Exception e) {
-            //Log.e("Buffer Error", "Error converting result " + e.toString());
-        	e.printStackTrace();
-        }
-        return "";
-	}*/
 }
