65520d9996dc506407ade2c00bb908ae7b0a533b
Marc Dojtschinov <marc.dojtschinov@students.unibe.ch>
2013-10-23 00:53:15 +0200

    Added Custom Favorite Toggle Button

--- Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/FavoriteOnClickListener.java
+++ Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/FavoriteOnClickListener.java
@@ -1,9 +1,6 @@
 package com.ese2013.mensaunibe;
 
-import java.util.ArrayList;
-
 import com.ese2013.mensaunibe.model.mensa.Mensa;
-
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ToggleButton;
@@ -20,15 +17,12 @@ public class FavoriteOnClickListener implements OnClickListener{
 	
 	@Override
 	public void onClick(View v) {
-		String strStatus= "";
 		if(favorite.isChecked()){
 		favorite.setChecked(false);
-		strStatus = "noFav";
 		mensa.setFavorite(false);
 		}
 		else{
 			favorite.setChecked(true);
-			strStatus = "fav";
 			mensa.setFavorite(true);
 		}
 		
