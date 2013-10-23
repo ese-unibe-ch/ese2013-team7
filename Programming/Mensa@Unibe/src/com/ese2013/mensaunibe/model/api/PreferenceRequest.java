083d43cea5f56fb0469f9113309140d25b45a988
Bugius <bugius1000@gmail.com>
2013-10-23 13:56:20 +0200

    Made Variables Private

--- Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/model/api/PreferenceRequest.java
+++ Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/model/api/PreferenceRequest.java
@@ -6,9 +6,8 @@ import android.content.SharedPreferences;
 import com.ese2013.mensaunibe.App;
 
 public class PreferenceRequest {
-	Context context = App.getAppContext();
-	SharedPreferences sharedPref = context.getSharedPreferences(
-			"com.ese2013.mensaunibe.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE);
+	private Context context = App.getAppContext();
+	private SharedPreferences sharedPref = context.getSharedPreferences("com.ese2013.mensaunibe.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE);
 	
 	public void writePreference(boolean toWrite, int mensaId) {
 		SharedPreferences.Editor editor = sharedPref.edit();
