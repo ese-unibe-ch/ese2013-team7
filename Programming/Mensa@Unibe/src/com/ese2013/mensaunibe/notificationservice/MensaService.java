aa0d54fc12b5f7ec246fd53c27ea80aea3658397
Marc Dojtschinov <marc.dojtschinov@students.unibe.ch>
2013-11-26 23:59:20 +0100

    Working on Mensa Service

---
+++ Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/notificationservice/MensaService.java
@@ -0,0 +1,19 @@
+package com.ese2013.mensaunibe.notificationservice;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MensaService extends IntentService {
+
+	public MensaService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
