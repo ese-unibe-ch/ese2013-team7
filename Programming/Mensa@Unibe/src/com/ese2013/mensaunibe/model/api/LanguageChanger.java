288d483fb9cd569ff14b607fbe1412323b65138a
Andreas Hohler <andreas.hohler@students.unibe.ch>
2013-12-01 18:26:26 +0100

    fixed crash on weekend menu translating

--- Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/model/api/LanguageChanger.java
+++ Programming/Mensa@Unibe/src/com/ese2013/mensaunibe/model/api/LanguageChanger.java
@@ -54,7 +54,7 @@ public class LanguageChanger extends AsyncTask<Void, Void, Boolean> {
 		
 		if(success) {
 			Toast.makeText(context, "Menus have been translated", Toast.LENGTH_SHORT).show();
-			adapter.notifyDataSetChanged();
+			if(adapter != null) adapter.notifyDataSetChanged();
 		} else {
 			Toast.makeText(context, "Menus could not have been translated", Toast.LENGTH_SHORT).show();
 		}
